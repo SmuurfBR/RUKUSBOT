4a90b23c6a6c382362745aca94370aa98086a2ab
SmuurfBR <39779447+SmuurfBR@users.noreply.github.com>
2018-06-11 11:17:24 -0300

    Update fortnite.js

--- comandos/prefix-r!/fortnite.js
+++ comandos/prefix-r!/fortnite.js
@@ -1,4 +1,4 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const config = require('../botconfig.json');
 const Fortnite = require('fortnite');
 const ftnApi = new Fortnite(botconfig.ftnApi);
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) =>{
     }
   }
 
-  if(!username)
+  if(`r!username`)
     return message.channel.send("Nome de usúario não fornecido. Use a syntax correta: `r!fortnite <epic-username> [plataforma pc/xbl/psn] {mode all/season}`.\n\Para lifetime stats use `r!fbr <epic-username> [plataforma pc/xbl/psn]`");
 
   let data = ftnApi.user(username, platform).then(data => {
@@ -168,56 +168,5 @@ module.exports.run = async (bot, message, args) =>{
 }
 
 module.exports.help = {
-  name: "fbr"
+  name: "fortnite"
 }
-=======
-+const Discord = require("discord.js");
-const config = require("../botconfig.json");
-const apikey = require("../keys.json");
-const Fortnite = require("fortnite");
-const ft = new Fortnite(apikey.fortnite);
-
-module.exports.run = async (bot, message, args) => {
-    message.delete();
-    //!fortnite SmuurfBR pc
-    let username = args[0];
-    let platform = args[1] || "pc";
-
-    let data = ft.getInfo(username, plataform).then(data => {
-
-        let switchtats = data.lifetimestats;
-        let kills = stats.find(s => s.stat == 'kills');
-        let wins = stats.find(s => s.stat == 'wins')
-        let kd = stats.find(s => s.stat == 'kd')
-        let mPlayed = stats.find(s => s.stat == 'matchesPlayed')
-        let tPlayed = stats.find(s => s.stat == 'timePlayed')
-        let asTime = stats.find(s => s.stat == 'avgSurvivalTime')
-
-        let embed = new Discord.RichEmbed
-        .setTitle("Fortnite Stats")
-        .setAuthor(data.username)
-        .setColor(config.orange)
-        .addField("Kills", kills.value, true)
-        .addField("Wins", wins.value, true)
-        .addField("K/D", kd.value, true)
-        .addField("Partidas Jogadas", mPlayed.value, true)
-        .addField("Tempo Jogado", tPlayed.value, true)
-        .addField("Average Survival Time", asTime.value, true)
-
-        message.channel.send(embed);
-
-
-      }).catch(e => {
-          console.log(e);
-          message.channel.send("Não foi possivel achar o usuário no sistema");
-      });
-
-
-
-
-  }
-
-  module.exports.help = {
-      name: "fortnite"
-  }
->>>>>>> 04275965c6bdf93faa0b26eea72301343a9454ab
