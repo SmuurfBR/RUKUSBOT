7c595193f3e21b1baf9ddf9580fbac30fb039669
SmuurfBR <39779447+SmuurfBR@users.noreply.github.com>
2018-06-11 19:44:15 -0300

    Update codigo.js

--- comandos/prefix-r!/codigo.js
+++ comandos/prefix-r!/codigo.js
@@ -1,5 +1,6 @@
+const botconfig = require("./botconfig.json")
 const Discord = require("discord.js")
-const botconfig = require("../../config.json")
+const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("message", async message => {
   if(message.author.bot) return;
@@ -10,7 +11,8 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if(cmd === `${prefix}botinfo`){
+  if(cmd === `${prefix}codigo`){
+    return message.channel.send("Código do bot feito por Márcio#1636");
+  }
 
-    return message.channel.send("Código do bot feito por Márcio#1636") ;
-  });
+});
