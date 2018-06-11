b043fe62a81169a8d89e9585eb58ba6b9a75df7e
SmuurfBR <39779447+SmuurfBR@users.noreply.github.com>
2018-06-11 19:54:43 -0300

    Update codigo.js

--- comandos/prefix-r!/codigo.js
+++ comandos/prefix-r!/codigo.js
@@ -5,12 +5,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-
-  let prefix = botconfig.prefix
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-
+  
  if(message.content == 'r!codigo') {
 
     message.channel.sendMessage('Código do bot feito por Márcio#1636');
